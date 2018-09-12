4a7d863f60592aae47131c45f1f41d5deb4b1f52
sabri <sabri@hamda.ch>
2018-09-12 21:32:39 +0200

    Install symfony/orm-pack, Create Trick Entity, Create trickRpository, Create p6 Database, Shema Update

---
+++ src/Entity/trick.php
@@ -0,0 +1,168 @@
+<?php
+/**
+ * Created by Sabri Hamda.
+ * Date: 12.09.18
+ * Time: 20:38
+ */
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\TrickRepository")
+ */
+class Trick
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $category;
+
+    /**
+     * @ORM\Column(type="string", length=26)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=126)
+     */
+    private $image;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $description;
+
+    /**
+     * @ORM\Column(type="datetime", length=26)
+     */
+    private $createdAt;
+
+    /**
+     * @ORM\Column(type="datetime", length=26)
+     */
+    private $updatedAt;
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id): void
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCategory()
+    {
+        return $this->category;
+    }
+
+    /**
+     * @param mixed $category
+     */
+    public function setCategory($category): void
+    {
+        $this->category = $category;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param mixed $name
+     */
+    public function setName($name): void
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getImage()
+    {
+        return $this->image;
+    }
+
+    /**
+     * @param mixed $image
+     */
+    public function setImage($image): void
+    {
+        $this->image = $image;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * @param mixed $description
+     */
+    public function setDescription($description): void
+    {
+        $this->description = $description;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * @param mixed $createdAt
+     */
+    public function setCreatedAt($createdAt): void
+    {
+        $this->createdAt = $createdAt;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getUpdatedAt()
+    {
+        return $this->updatedAt;
+    }
+
+    /**
+     * @param mixed $updatedAt
+     */
+    public function setUpdatedAt($updatedAt): void
+    {
+        $this->updatedAt = $updatedAt;
+    }
+
+
+
+}
---
+++ src/Repository/TrickRepository.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by Sabri Hamda.
+ * Date: 12.09.18
+ * Time: 21:07
+ */
+
+namespace App\Repository;
+
+
+class TrickRepository
+{
+
+}
