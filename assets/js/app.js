1ef62dd812e4ea8daf96472b17d0b2a88fc3e577
sabri <sabri@hamda.ch>
2018-09-12 20:23:40 +0200

    Create nav, content, trik list

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,11 @@
 const $ = require('jquery');
 
 require('bootstrap');
+
+$(document).ready(function(){
+    $(window).scroll(function() {
+        var scrollTop = $(window).scrollTop();
+        var imgPos = (scrollTop / -2 + 'px');
+        $('.main-header').css('transform', 'translateY(' + imgPos + ')');
+    });
+});
