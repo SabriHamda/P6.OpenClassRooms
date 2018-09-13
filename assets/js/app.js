3fc1258592ac0c7a5eba736a76506adc2bf29630
sabri <sabri@hamda.ch>
2018-09-13 11:26:21 +0200

    Create parallax effect

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,6 +6,10 @@ $(document).ready(function(){
     $(window).scroll(function() {
         var scrollTop = $(window).scrollTop();
         var imgPos = (scrollTop / -2 + 'px');
+        var titlePos = (scrollTop / -1 + 'px');
+
         $('.main-header').css('transform', 'translateY(' + imgPos + ')');
+        $('.header-title').css('transform', 'translateX(' + titlePos + ')');
+
     });
 });
