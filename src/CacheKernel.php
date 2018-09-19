7a1ff7beacf5e9ca188fbd0d6b3b07eb5f0475bb
sabri <sabri@hamda.ch>
2018-09-19 22:15:21 +0200

    Create HTTP proxy & Use http cache on home page

---
+++ src/CacheKernel.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by Sabri Hamda.
+ * Date: 19.09.18
+ * Time: 12:36
+ */
+
+namespace App;
+
+
+use Symfony\Bundle\FrameworkBundle\HttpCache\HttpCache;
+
+class CacheKernel extends HttpCache
+{
+
+}
