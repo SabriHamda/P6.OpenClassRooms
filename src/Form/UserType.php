57f1dbdefdcd5d4846a8049b563a8c82f2c738a2
sabri <sabri@hamda.ch>
2018-09-27 15:06:05 +0200

    Create UserType

---
+++ src/Form/UserType.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Created by Sabri Hamda.
+ * Date: 26.09.18
+ * Time: 12:40
+ */
+
+namespace App\Form;
+
+use App\Domain\DTO\UserRegistrationDTO;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+
+class UserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('email', EmailType::class)
+            ->add('username', TextType::class)
+            ->add('password', RepeatedType::class, array(
+                'type' => PasswordType::class,
+                'first_options'  => array('label' => 'Password'),
+                'second_options' => array('label' => 'Repeat Password'),
+            ))
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => UserRegistrationDTO::class,
+            'empty_data' => function(FormInterface $form){
+                return new UserRegistrationDTO(
+                    $form->get('username')->getData(),
+                    $form->get('email')->getData(),
+                    $form->get('password')->getData()
+                );
+
+            }
+        ));
+    }
+}
