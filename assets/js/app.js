a320197bad7c64f5c839e50eaa3df21e251166f3
sabri <sabri@hamda.ch>
2018-09-30 10:56:24 +0200

    Create popin to show messages

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,7 +2,7 @@ const $ = require('jquery');
 
 require('bootstrap');
 
-window.addEventListener('scroll', function(e){
+window.addEventListener('scroll', function (e) {
     let windowScrollTop = this.scrollY;
     let imgPos = (windowScrollTop / -2 + 'px');
     let titlePos = (windowScrollTop / -1 + 'px');
@@ -11,3 +11,15 @@ window.addEventListener('scroll', function(e){
     document.getElementById('header-title').style.transform = "translate(" + titlePos + " ,0)";
 
 });
+
+document.getElementById('message-btn').addEventListener('click', function (event) {
+    event.preventDefault();
+    hide(document.querySelectorAll('.message'));
+});
+
+function hide(elements) {
+    elements = elements.length ? elements : [elements];
+    for (let index = 0; index < elements.length; index++) {
+        elements[index].style.display = 'none';
+    }
+}
