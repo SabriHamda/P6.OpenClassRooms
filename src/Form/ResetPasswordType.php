2942737b31dd9ee93392e2bee852d7c2f5a71400
sabri <sabri@hamda.ch>
2018-10-10 00:38:55 +0200

    Create reset Password Form

---
+++ src/Form/ResetPasswordType.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Created by Sabri Hamda <sabri@hamda.ch>
+ */
+
+namespace App\Form;
+
+use App\Domain\DTO\ResetPasswordDTO;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+
+class ResetPasswordType extends AbstractType
+{
+
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('email', EmailType::class);
+
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => ResetPasswordDTO::class,
+            'empty_data' => function (FormInterface $form) {
+                return new ResetPasswordDTO(
+                    $form->get('email')->getData()
+                );
+            },
+            'validation_groups' => ['ResetPasswordDTO']
+        ));
+    }
+}
