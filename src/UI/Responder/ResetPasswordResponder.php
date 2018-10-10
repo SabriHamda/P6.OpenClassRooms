c543d6c0a9859fd619f2b59c745a9ce8e69c25b0
sabri <sabri@hamda.ch>
2018-10-10 13:02:25 +0200

    Create the responder

---
+++ src/UI/Responder/ResetPasswordResponder.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Created by Sabri Hamda <sabri@hamda.ch>
+ */
+
+namespace App\UI\Responder;
+
+
+use App\UI\Responder\Interfaces\ResetPasswordResponderInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Twig\Environment;
+
+class ResetPasswordResponder implements ResetPasswordResponderInterface
+{
+
+    /**
+     * @var Environment
+     */
+    private $twig;
+
+    /**
+     * ResetTokenResponder constructor.
+     * @param Environment $twig
+     */
+    public function __construct(Environment $twig)
+    {
+        $this->twig = $twig;
+    }
+
+    /**
+     * @param Request $request
+     * @param $viewForm
+     * @return Response
+     * @throws \Twig_Error_Loader
+     * @throws \Twig_Error_Runtime
+     * @throws \Twig_Error_Syntax
+     */
+    public function __invoke(Request $request, $viewForm): Response
+    {
+        $response = new Response($this->twig->render('frontend/resetPassword.html.twig',['resetPasswordForm'=> $viewForm,'moi'=> $moi]));
+        return $response;
+    }
+}
