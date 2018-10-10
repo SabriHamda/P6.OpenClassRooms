447815e854dd7f96fa639a88563f20069872a7c8
sabri <sabri@hamda.ch>
2018-10-10 13:01:56 +0200

    Create the responder of resetPassword action

---
+++ src/UI/Responder/Interfaces/ResetPasswordResponderInterface.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Created by Sabri Hamda <sabri@hamda.ch>
+ */
+
+namespace App\UI\Responder\Interfaces;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Twig\Environment;
+
+/**
+ * Interface ResetTokenResponderInterface
+ * @package App\UI\Responder\Interfaces
+ */
+interface ResetPasswordResponderInterface
+{
+    /**
+     * ResetTokenResponderInterface constructor.
+     * @param Environment $twig
+     */
+    public function __construct(Environment $twig);
+
+    /**
+     * @param Request $request
+     * @param $viewForm
+     * @return Response
+     */
+    public function __invoke(Request $request, $viewForm) :Response;
+
+}
