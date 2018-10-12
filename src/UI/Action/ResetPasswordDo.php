90ff945ec4297784585d36261dc4e64a84d4e022
sabri <sabri@hamda.ch>
2018-10-12 11:24:08 +0200

    Check if user exists in db by reset_token -> Create the form view -> Check if email is valid -> Validator check constraints -> Encrypt the new password -> Insert password in db -> Remove reset_token -> Add a new date in updated_at column

--- src/UI/Action/ResetPasswordDo.php
+++ src/UI/Action/ResetPasswordDo.php
@@ -5,6 +5,7 @@
 
 namespace App\UI\Action;
 
+use App\Entity\User;
 use App\Form\ResetPasswordDoType;
 use App\Repository\Interfaces\UserRepositoryInterface;
 use App\UI\Responder\Interfaces\ResetPasswordDoResponderInterface;
@@ -13,8 +14,16 @@ use Symfony\Component\Form\FormFactoryInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Security\Core\Encoder\EncoderFactoryInterface;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+use Symfony\Component\HttpFoundation\Session\Flash\FlashBagInterface;
+
 use Twig\Environment;
 
+/**
+ * Class ResetPasswordDo
+ * @package App\UI\Action
+ */
 class ResetPasswordDo
 {
 
@@ -23,22 +32,43 @@ class ResetPasswordDo
      */
     private $twig;
 
+    /**
+     * @var UserRepositoryInterface
+     */
     private $userRepository;
 
+    /**
+     * @var FormFactoryInterface
+     */
     private $formFactory;
 
+    /**
+     * @var UrlGeneratorInterface
+     */
     private $redirectRoute;
+
+    /**
+     * @var ValidatorInterface
+     */
+    private $validator;
+
+    /**
+     * @var FlashBag
+     */
+    private $flash;
 
     /**
      * ResetPasswordDo constructor.
      * @param $twig
      */
-    public function __construct(Environment $twig,UserRepositoryInterface $userRepository,FormFactoryInterface $formFactory, UrlGeneratorInterface $urlGenerator)
+    public function __construct(Environment $twig, UserRepositoryInterface $userRepository, FormFactoryInterface $formFactory, UrlGeneratorInterface $urlGenerator, ValidatorInterface $validator, FlashBagInterface $flash)
     {
         $this->twig = $twig;
         $this->userRepository = $userRepository;
         $this->formFactory = $formFactory;
         $this->redirectRoute = $urlGenerator;
+        $this->validator = $validator;
+        $this->flash = $flash;
     }
 
     /**
@@ -48,17 +78,34 @@ class ResetPasswordDo
      * @param $token
      * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function __invoke(ResetPasswordDoResponderInterface $responder,Request $request,$token)
+    public function __invoke(ResetPasswordDoResponderInterface $responder, Request $request, EncoderFactoryInterface $encoderFactory, $token)
     {
-        $user = $this->userRepository->findOneBy(['resetPasswordToken'=> $token]);
-        if ($user){
-            $form = $this->formFactory->createNamed('resetPasswordForm',ResetPasswordDoType::class)->handleRequest($request);
+        $user = $this->userRepository->findOneBy(['resetPasswordToken' => $token]);
+        if ($user) {
+            $form = $this->formFactory->createNamed('resetPasswordForm', ResetPasswordDoType::class)->handleRequest($request);
             $viewForm = $form->createView();
-            return $responder($request,$viewForm);
-        }else{
+            if ($form->isSubmitted() && $form->isValid()) {
+                $encryptedPassword = $encoderFactory->getEncoder(User::class)->encodePassword($form->getData()->password, null);
+                // Use validator to validate form
+
+                $errors = $this->validator->validate($form);
+
+                if (count($errors) > 0) {
+                    $viewForm = $form->createView();
+                    return $responder($request, $viewForm, $errors);
+                } else {
+                    $user->updatePassword($encryptedPassword);
+                    $this->userRepository->update();
+                    $this->flash->add('success', 'Votre mot de passe a bien été modifié, vous pouvez dès à présent vous connecter en cliquant <a href="/login">ici</a>.');
+                    return new RedirectResponse($this->redirectRoute->generate('home'));
+                }
+            }
+            return $responder($request, $viewForm);
+        } else {
             return new RedirectResponse($this->redirectRoute->generate('home'));
 
         }
+
 
     }
 }
